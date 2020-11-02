44d4466a19c73519ed82b9175ba9ea5f5b8e220d
João Rafael <jrsc2@cin.ufpe.br>
2020-11-02 15:58:07 -0300

    Port forwarding and dashboard setup

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ sudo chmod +x ./elk-setup.sh
 ./elk-setup.sh install filebeat
 ./elk-setup.sh install metricbeat
 
+minikube addons enable metrics-server
+minikube addons enable dashboard
+
 helm install promstack kube-prometheus-stack-10.1.1.tgz
+kubectl -f apply elk/crawler-cronjob.yaml
+
 sudo chmod +x ./port-forwarding
 ./port-forwarding.sh &
-
-kubectl -f apply elk/crawler-cronjob.yaml
